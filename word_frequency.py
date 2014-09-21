f2c548ea3a1f5f7def583c936bfc0a3b11306177
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-09-21 16:43:18 -0400

    Broke out testing function into test_cmp. Began to add testing to histogram

--- word_frequency.py
+++ word_frequency.py
@@ -13,39 +13,61 @@ import threading
 
 
 class SafeCount:
-"""SafeCount, a wrapper for an object, similar to a semaphore, 
-that provides thread-safe access to a countable integer 
-(this use case: counts a particular word's frequency)"""    
+    """SafeCount, a wrapper for an object, similar to a semaphore, 
+    that provides thread-safe access to a countable integer 
+    (this use case: counts a particular word's frequency)"""    
     def __init__(self):
         self.count = 0
         self.count_lock = threading.Lock()
 
     def increment(self):
-        self.lock.acquire()
+        self.count_lock.acquire()
         self.count += 1
-        self.lock.release()
+        self.count_lock.release()
 
     def decrement(self):
-        self.lock.acquire()
-        self.count += 1
-        self.lock.release()
+        self.count_lock.acquire()
+        self.count -= 1
+        self.count_lock.release()
 
     def reset(self, new_count=0):
-        self.lock.acquire()
+        self.count_lock.acquire()
         self.count = new_count
-        self.lock.release()
+        self.count_lock.release()
 
     def get_count(self):
-        self.lock.acquire()
+        self.count_lock.acquire()
         count = self.count
-        self.lock.release()
+        self.count_lock.release()
         return count
 
 class Histogram:
-"""Histogram, a class that wraps and provides thread-safe access to 
-a dict whose keys are strings and whose objects are SafeCounts"""
+    """Histogram, a class that wraps and provides thread-safe access to 
+    a dict whose keys are strings and whose objects are SafeCounts"""
     def __init__(self):
         self.dictionary = dict()
         self.dict_lock = threading.Lock()
 
     def increase_count(self, word):
+        word = word.lower()
+        word = word.strip()
+        self.dict_lock.acquire()
+        if word in self.dictionary:
+            self.dictionary[word].increment()
+        else:
+            self.dictionary[word] = SafeCount()
+            self.dictionary[word].increment()
+        self.dict_lock.release()
+
+    def get_count(self, word):
+        word = word.lower()
+        word = word.strip()        
+        self.dict_lock.acquire()
+        return_count = 0
+        if word in self.dictionary:
+            return_count = self.dictionary[word].get_count()
+        self.dict_lock.release()
+        return return_count
+
+
+
--- word_frequency_testing.py
+++ word_frequency_testing.py
@@ -5,31 +5,52 @@
 # 9/25/14
 # COMP50 Concurrant Programming
 
+import sys
 import threading
 import word_frequency
 
 """word_frequency_testing.py, a testing environment for module
 word_frequency.py"""
 
+tests_failed_string = ""
+
+def test_cmp (test_value, correct_value, test_name):
+    if test_value == correct_value:
+        return True
+    else:
+        tests_failed_string += test_name
+        tests_failed_string += " Failed: {0} != {1}\n"\
+                                            .format(test_value, correct_value)
+        return False
+
 def main():
     tests_passed = True
     tests_failed_string = ""
     example_safecount = word_frequency.SafeCount()
     
-    #TEST 1
+    #TEST SIMPLE INCREMENT
     example_safecount.increment()
     example_safecount.increment()
-    if example_safecount.get_count() != 2:
-        tests_passed = False
-        tests_failed_string += "Simple increment \n"
+    tests_passed = tests_passed or test_cmp(example_safecount.get_count(), 2, "Simple Increment")
 
-    #TEST 2
+    #TEST SIMPLE RESET
+    example_safecount.reset(5)
+    tests_passed = tests_passed or test_cmp(example_safecount.get_count(), 5, "Simple Reset")
+
+    #TEST SIMPLE DECREMENT
     example_safecount.reset(5)
     example_safecount.decrement()
     example_safecount.decrement()
-    if example_safecount.get_count() != 3:
-        tests_passed = False
-        tests_failed_string += "Simple decrement"
+    tests_passed = tests_passed or test_cmp(example_safecount.get_count(), 3, "Simple Decrement")
+
+    #TEST SIMPLE HISTOGRAM
+    example_histogram = word_frequency.Histogram()
+
+    example_histogram.increase_count("Dummy")
+    example_histogram.increase_count("dummy")
+    example_histogram.increase_count("dummy ")
+
+    tests_passed = tests_passed or test_cmp(example_histogram.get_count("dummy"), 3, "Simple Word Adding")
 
 
     if tests_passed:
@@ -39,4 +60,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
